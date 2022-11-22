7cf6fe253f3da062e7260c890d1bd7e68d1d61db
Nicole <nicole.georgievva@gmail.com>
2022-11-22 17:12:04 +0200

    Create a mutable state incrementing a counter

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.introtocompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -11,12 +12,15 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,30 +45,42 @@ fun MyApp() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = "$100", style = TextStyle(
+                    color = Color.White,
+                    fontSize = 35.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            Spacer(modifier = Modifier.height(130.dp))
             CreateCircle()
         }
     }
 }
 
-@Preview
 @Composable
 fun CreateCircle() {
+    var moneyCounter by remember {
+        mutableStateOf(0)
+    }
+
     Card(
         modifier = Modifier
             .padding(3.dp)
             .size(105.dp)
             .clickable {
+                moneyCounter += 1
             },
         shape = CircleShape,
         elevation = 4.dp
     ) {
         Box(contentAlignment = Alignment.Center) {
-            Text(text = "Tap", modifier = Modifier)
+            Text(text = "Tap $moneyCounter", modifier = Modifier)
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
     IntroToComposeTheme {
